c001eacbcea0aef8455271531a1671af8bf4c959
JW <jacekwoln@gmail.com>
2023-08-22 19:58:46 +0200

    Remove old test

--- Client/src/test/java/io/github/jwdeveloper/tiktok/ParseMessagesTests.java
+++ Client/src/test/java/io/github/jwdeveloper/tiktok/ParseMessagesTests.java
@@ -4,21 +4,11 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import io.github.jwdeveloper.tiktok.messages.WebcastChatMessage;
 import io.github.jwdeveloper.tiktok.messages.WebcastGiftMessage;
 import io.github.jwdeveloper.tiktok.messages.WebcastLikeMessage;
-import io.github.jwdeveloper.tiktok.messages.WebcastWebsocketMessage;
 import org.junit.Test;
 
 public class ParseMessagesTests extends TikTokBaseTest
 {
 
-
-  //  @Test
-    public void ShouldParseWebcastWebsocketMessage() throws InvalidProtocolBufferException {
-        var bytes = getFileBytesUtf("WebcastWebsocketMessage.bin");
-        var message = WebcastWebsocketMessage.parseFrom(bytes);
-        System.out.println("id: " + message.getId());
-        System.out.println("type: " + message.getType());
-        System.out.println("binary: " + message.getBinary().size());
-    }
 
     @Test
     public void ShouldParseLikeMessage() throws InvalidProtocolBufferException {
