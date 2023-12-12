82990665b8bb81adb9d5d417625c02a6e25b1d52
JW <jacekwoln@gmail.com>
2023-12-12 19:38:55 +0100

    Changes:   updated structure of WebcastLinkLayerMessage   in proto file

--- Examples/src/main/java/io/github/jwdeveloper/tiktok/Examplee.java
+++ Examples/src/main/java/io/github/jwdeveloper/tiktok/Examplee.java
@@ -20,34 +20,16 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package io.github.jwdeveloper.tiktok.utils;
+package io.github.jwdeveloper.tiktok;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-
-public class ProtoBufferJsonGenerator {
-    public static JsonObject genratejson(ProtoBufferObject protoBuffObj) {
-
-        JsonObject jsonObject = new JsonObject();
-        for (var entry : protoBuffObj.getFields().entrySet()) {
-            var fieldName = entry.getKey() + "_" + entry.getValue().type;
-            if (entry.getValue().value instanceof ProtoBufferObject protoObj)
-            {
-                JsonObject childJson = genratejson(protoObj);
-                jsonObject.add(fieldName, childJson);
-                continue;
-            }
-
-            var value = entry.getValue().value.toString();
-            jsonObject.addProperty(fieldName, value);
-        }
-
-        return jsonObject;
-    }
-
-    public static String generate(ProtoBufferObject protoBufferObject) {
-        var json = genratejson(protoBufferObject);
-        var gson = new GsonBuilder().setPrettyPrinting().create();
-        return gson.toJson(json);
+public class Examplee
+{
+    public static void main(String[] args)
+    {
+        TikTokLive.newClient("skullchefasmr")
+                .onGift((liveClient, event) ->
+                {
+                    System.out.println("Dzięki za gifta "+event.getGift().getName());
+                }).buildAndConnect();
     }
 }
